30c2313f9f4e7e8c4bfcd99b52f7813de91037dc
SalmanMKC <32169182+salmanmkc@users.noreply.github.com>
2019-02-09 13:23:38 +0000

    Using Linq to get the 5 largest files in directory, using string interpolation as well

--- LargestFilesInDirectory/Program.cs
+++ LargestFilesInDirectory/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LargestFilesInDirectory
+namespace FileDirectory
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string path = @"c:\windows";
+            largestFiles(path);
+            Console.ReadLine();
+        }
+
+
+        private static void largestFiles(string path)
+        {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+            foreach (var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0} ");
+            }
         }
     }
 }
